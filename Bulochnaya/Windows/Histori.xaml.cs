5a5b69ac6d887397ca0acfb721fcb481cfe723b3
булат шакиров <a2009-1@mail.ru>
2022-05-04 15:04:52 +0300

    fix

--- Bulochnaya/Windows/Histori.xaml.cs
+++ Bulochnaya/Windows/Histori.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Bulochnaya.Windows
 {
@@ -24,7 +13,6 @@ namespace Bulochnaya.Windows
         {
             InitializeComponent();
             Grof.ItemsSource = inHistori.Projects.ToList();
-            string lol = DateTime.Now.ToString();
         }
     }
 }
